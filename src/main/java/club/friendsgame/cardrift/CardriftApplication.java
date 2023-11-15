25d487d430461dbfc8a84be2bf672faeea0ba43c
aggarrohit <32900818+aggarrohit@users.noreply.github.com>
2023-11-15 11:05:53 +0100

    initial setup

---
+++ src/main/java/club/friendsgame/cardrift/CardriftApplication.java
@@ -0,0 +1,13 @@
+package club.friendsgame.cardrift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardriftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardriftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/friendsgame/cardrift/CardriftApplicationTests.java
@@ -0,0 +1,13 @@
+package club.friendsgame.cardrift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardriftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
